28e697202628a8c4176f952df6125bdfed41eab5
James Nylen <jnylen@gmail.com>
2018-11-14 02:03:36 -0500

    Add a way to update the upgrades API (JSON blobs)

--- build-and-log.sh
+++ build-and-log.sh
@@ -5,4 +5,20 @@
 	code=$?
 	echo
 	echo "Exit code: $code"
+
+	if [ $code -eq 0 ]; then
+		echo
+		. "$(dirname "$0")/config.sh"
+		if [ -x "$UPGRADE_API_SCRIPT" ]; then
+			echo "Running upgrade API script:"
+			echo
+			"$UPGRADE_API_SCRIPT"
+			code=$?
+			echo
+			echo "Exit code: $code"
+		else
+			echo "Upgrade API script not found:"
+			echo "'$UPGRADE_API_SCRIPT'"
+		fi
+	fi
 ) > /tmp/nightly-build.log 2>&1
--- example-config.sh
+++ example-config.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 GITHUB_API_TOKEN="token" # needs 'repo' scope
+UPGRADE_API_SCRIPT="/path/to/api/v1-upgrade-generator/update.sh"
